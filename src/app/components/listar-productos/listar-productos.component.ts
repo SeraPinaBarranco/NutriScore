4351f57c5c49789108f7957ac10db0d6eface7eb
SeraPinaBarranco <arespin23@gmail.com>
2021-07-08 14:45:07 +0200

    DataSource solucionado!

--- src/app/components/listar-productos/listar-productos.component.ts
+++ src/app/components/listar-productos/listar-productos.component.ts
@@ -3,22 +3,29 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Productos } from 'src/app/models/productos.model';
 import { ProductoService } from 'src/app/services/producto.service';
 
-
-
 @Component({
   selector: 'app-listar-productos',
   templateUrl: './listar-productos.component.html',
   styleUrls: ['./listar-productos.component.css'],
 })
-
 export class ListarProductosComponent implements OnInit {
   product: Productos;
   listaProductos: Productos[] = [];
 
-  displayedColumns: string[] = ['nombre'/*, 'calorias', 'proteinas', 'grasas','hidratos'*/];
-  dataSource = new MatTableDataSource();
+  columnas: string[] = [
+    'nombre',
+    'calorias',
+    'proteinas',
+    'grasas',
+    'hidratos'
+    //'id',
+  ];
+  //dataSource = new MatTableDataSource();
 
-  
+  //dataSource= ELEMENT_DATA;
+
+  //dataSource= new MatTableDataSource<Productos>();
+  public dataSource: any[] = [];
   constructor(private _productoService: ProductoService) {}
 
   ngOnInit(): void {
@@ -26,23 +33,60 @@ export class ListarProductosComponent implements OnInit {
   }
 
   llenarArrayProductos() {
+    let s = 0;
     //Trae todos los registros del Service
     this._productoService.obternerProductos().subscribe((res) => {
       res.forEach((p: any) => {
-        this.product = new Productos(
-          p.payload.doc.data().nombre,
-          p.payload.doc.data().calorias,
-          p.payload.doc.data().proteinas,
-          p.payload.doc.data().grasas,
-          p.payload.doc.data().hidratos,
-          p.payload.doc.id
-        );
+        this.product = {
+          nombre: p.payload.doc.data().nombre,
 
+          calorias: p.payload.doc.data().calorias,
+          proteinas: p.payload.doc.data().proteinas,
+          grasas: p.payload.doc.data().grasas,
+          hidratos: p.payload.doc.data().hidratos,
+          id: p.payload.doc.id,
+        };
         this.listaProductos.push(this.product);
       });
+
+      this.dataSource = this.listaProductos;
     });
     
-    this.dataSource.data = this.listaProductos; 
-    console.log(this.dataSource.data);
   }
 }
+
+/*export interface Prod {
+  nombre: string;
+  calorias: number;
+  proteinas: number;
+  grasas: number;
+  hidratos: number;
+  id: string;
+}
+
+const ELEMENT_DATA: Prod[] = [
+  {
+    nombre: 'ww',
+    calorias: 2,
+    proteinas: 23,
+    grasas: 44,
+    hidratos: 63,
+    id: 'w23ddw',
+  },
+  {
+    nombre: 'sw',
+    calorias: 1,
+    proteinas: 223,
+    grasas: 74,
+    hidratos: 6,
+    id: 'ertrdg',
+  },
+  {
+    nombre: 'ttw',
+    calorias: 12,
+    proteinas: 2223,
+    grasas: 94,
+    hidratos: 16,
+    id: 'gnvnv',
+  },
+];*/
