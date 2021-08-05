9be9e59402753a4d7582868af19fc2428e1c07b1
SeraPinaBarranco <arespin23@gmail.com>
2021-08-05 14:42:11 +0200

    add ingrediente

--- src/app/components/crear-recetas/crear-recetas.component.ts
+++ src/app/components/crear-recetas/crear-recetas.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-recetas',
   templateUrl: './crear-recetas.component.html',
-  styleUrls: ['./crear-recetas.component.css']
+  styleUrls: ['./crear-recetas.component.css'],
 })
 export class CrearRecetasComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+  formularioValido: boolean = false;
+  mostrarAddIng: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.crearForm();
   }
 
+  crearReceta() {
+    if(this.form.valid){
+      this.formularioValido= true;
+    }else{
+      this.formularioValido= false
+    }    
+  }
+
+  crearForm() {
+    this.form = new FormGroup({
+      nombre: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(15),
+      ]),
+    });
+  }
+
+  mostrarAddIngrediente(){
+    this.mostrarAddIng = true;
+    if(this.formularioValido && this.mostrarAddIng){
+      
+    }
+  }
 }
