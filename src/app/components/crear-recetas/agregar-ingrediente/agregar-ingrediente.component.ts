4bcdc836917be002ea2e028c4c4130579a7155e1
SeraPinaBarranco <arespin23@gmail.com>
2021-08-06 14:07:42 +0200

    Select add ingrediente

--- src/app/components/crear-recetas/agregar-ingrediente/agregar-ingrediente.component.ts
+++ src/app/components/crear-recetas/agregar-ingrediente/agregar-ingrediente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Productos } from 'src/app/models/productos.model';
+import { ProductoService } from '../../../services/producto.service';
 
 @Component({
   selector: 'app-agregar-ingrediente',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-ingrediente.component.css']
 })
 export class AgregarIngredienteComponent implements OnInit {
+  
+  productos: Productos[] = [];
 
-  constructor() { }
+  constructor(private _productoSevice: ProductoService) { }
 
   ngOnInit(): void {
+    this._productoSevice.obternerProductos().subscribe(data =>{
+      let producto: any;
+
+      data.forEach((p:any) =>{
+        producto={
+          nombre : p.payload.doc.data().nombre,
+          calorias:p.payload.doc.data().calorias,
+          proteinas:p.payload.doc.data().proteinas,
+          grasas:p.payload.doc.data().grasas,
+          hidratos:p.payload.doc.data().hidratos
+        }
+
+        this.productos.push(producto);
+      })
+    });
+    console.log(this.productos);
   }
 
 }
