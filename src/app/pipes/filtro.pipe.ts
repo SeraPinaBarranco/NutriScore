65c6d5f6366b29c93b75e0f7e11577dddaf4f462
Sera Pina Barranco <arespin23@gmail.com>
2021-08-31 21:23:01 +0200

    filtro mejorado

--- src/app/pipes/filtro.pipe.ts
+++ src/app/pipes/filtro.pipe.ts
@@ -1,22 +1,30 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Productos } from '../models/productos.model';
 
 @Pipe({
   name: 'filtro'
 })
 export class FiltroPipe implements PipeTransform {
 
-  transform(value: any, arg: any,pagina:number): any {
-    const resultadoFiltro= [];
+  transform(value: Productos[], filtro: string,pagina:number): Productos[] {
+    //let resultadoFiltro: Productos[]= [];
     //arg es lo que se escribe
     //value es el array
 
-    for(const resultado of value){
+    /*for(const resultado of value){
       if(resultado.nombre.toLowerCase().indexOf(arg.toLowerCase()) > -1){
         resultadoFiltro.push(resultado);
       }
-    }
+    }*/
     console.log(pagina);
-    return resultadoFiltro;
+    if(filtro.length === 0) return value = value.slice(pagina, pagina + 2);
+
+    //resultadoFiltro = value.slice(pagina, pagina + 2);
+
+    //
+    const resultadoFiltro = value.filter( prod => prod.nombre.includes(filtro));
+
+    return resultadoFiltro.slice(pagina, pagina + 2);
   }
 
 }
